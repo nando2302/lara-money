60f75dc5313a1cc79a41a7c9d57e6cb2797ebe33
fernnado mardi nurzaman <fernandomardi1@gmail.com>
2022-06-10 19:05:01 +0700

    menambahkan dashboard dan company profile model dan database set

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\CompanyProfile;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard',function () {
+    return view('layouts.dashboard');
+});
+Route::resource('/companyprofile',CompanyProfile::class);
+
